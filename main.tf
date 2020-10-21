cb154d0cac8cb7cf7b8a05a47670d38a2d09c215
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-10-21 21:47:16 +0200

    Create main.tf
    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_vpc" "aws_vpc" {
+  cidr_block       = "10.199.199.0/24"
+  tags = {
+      Name = "aws-us-east1-spoke1"
+    }
+}
