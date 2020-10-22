b87045eb83de76e8be4d405b61c430a09e812de2
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-10-22 09:54:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "aviatrix" {
-   username     = "admin"
-  password      = "Password123"
+  username     = "admin"
+  password      = var.ctrl_password
+  controller_ip = "35.171.31.227"
+  version       = "2.16.3"
 }
+
+
 
 resource "aviatrix_vpc" "aws_vpc" {
   cloud_type           = 1
