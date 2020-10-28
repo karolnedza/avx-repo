e024575510e0c90b04d4ccf41a621a460f47193a
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-10-28 18:22:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aviatrix_vpc" "aws_vpc" {
   cloud_type           = 1
   account_name         = "aws-account"
   region               = "eu-west-1"
-  name                 = "Servicenow_VPC"
+  name                 = "Servicenow-VPC"
   cidr                 = var.pet_name_length
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
