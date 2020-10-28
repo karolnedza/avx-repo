3b24fec3ee641e04a42aaf841d60319c8ad22412
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-10-28 17:10:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aviatrix" {
   username     = "admin"
-  password      = var.ctrl_password
+  password      = var.example
   controller_ip = "35.171.31.227"
   version       = "2.17.0"
 }
@@ -12,7 +12,7 @@ resource "aviatrix_vpc" "aws_vpc" {
   account_name         = "aws-account"
   region               = "eu-west-1"
   name                 = "Servicenow VPC"
-  cidr                 = var.vpc_cidr
+  cidr                 = var.pet_name_length
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
 }
